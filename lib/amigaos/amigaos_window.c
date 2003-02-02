1d82fd35abf8837de1e7fc1752b3f0c567ecdd20
Marcus Geelnard <marcus256@users.sourceforge.net>
2003-02-02 22:23:09 +0000

    Updated file header, which now includes CVS information.

--- lib/amigaos/amigaos_window.c
+++ lib/amigaos/amigaos_window.c
@@ -1,11 +1,10 @@
 //========================================================================
 // GLFW - An OpenGL framework
-// File:     amigaos_window.c
-// Platform: AmigaOS (3.x)
-// Version:  2.4
-// Date:     2003.01.08
-// Author:   Marcus Geelnard (marcus.geelnard@home.se)
-// WWW:      http://hem.passagen.se/opengl/glfw/
+// File:        amigaos_window.c
+// Platforms:   AmigaOS, MorphOS
+// API version: 2.4
+// Author:      Marcus Geelnard (marcus.geelnard at home.se)
+// WWW:         http://glfw.sourceforge.net
 //------------------------------------------------------------------------
 // Copyright (c) 2002-2003 Marcus Geelnard
 //
@@ -28,7 +27,9 @@
 //    distribution of GLFW.
 //
 // Marcus Geelnard
-// marcus.geelnard@home.se
+// marcus.geelnard at home.se
+//------------------------------------------------------------------------
+// $Id: amigaos_window.c,v 1.2 2003-02-02 22:20:54 marcus256 Exp $
 //========================================================================
 
 #include "internal.h"
--- lib/amigaos/platform.h
+++ lib/amigaos/platform.h
@@ -1,11 +1,10 @@
 //========================================================================
 // GLFW - An OpenGL framework
-// File:     platform.h
-// Platform: AmigaOS (3.x)
-// Version:  2.4
-// Date:     2003.01.08
-// Author:   Marcus Geelnard (marcus.geelnard@home.se)
-// WWW:      http://hem.passagen.se/opengl/glfw/
+// File:        platform.h
+// Platforms:   AmigaOS, MorphOS
+// API version: 2.4
+// Author:      Marcus Geelnard (marcus.geelnard at home.se)
+// WWW:         http://glfw.sourceforge.net
 //------------------------------------------------------------------------
 // Copyright (c) 2002-2003 Marcus Geelnard
 //
@@ -28,7 +27,9 @@
 //    distribution of GLFW.
 //
 // Marcus Geelnard
-// marcus.geelnard@home.se
+// marcus.geelnard at home.se
+//------------------------------------------------------------------------
+// $Id: platform.h,v 1.2 2003-02-02 22:20:54 marcus256 Exp $
 //========================================================================
 
 #ifndef _platform_h_
@@ -51,11 +52,6 @@
   #define _GLFW_STORMMESA
 #endif
 
-// If this is the file where we keep the library bases, don't let the
-// AmigaOS include files declare them as external
-#if defined( _init_c_ )
-  #define __NOLIBBASE__
-#endif
 
 
 // Include files
