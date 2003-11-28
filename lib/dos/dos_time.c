7e4031bc8a7e4bc4f52a39dd0f32206f7eada442
Marcus Geelnard <marcus256@users.sourceforge.net>
2003-11-28 13:50:12 +0000

    Added some code for RDTSC support

--- lib/dos/dos_time.c
+++ lib/dos/dos_time.c
@@ -30,16 +30,178 @@
 // Marcus Geelnard
 // marcus.geelnard at home.se
 //------------------------------------------------------------------------
-// $Id: dos_time.c,v 1.1 2003-11-26 20:45:03 marcus256 Exp $
+// $Id: dos_time.c,v 1.2 2003-11-28 13:50:12 marcus256 Exp $
 //========================================================================
 
 #include "internal.h"
+
+
+// We use the __i386 define later in the code. Check if there are any
+// other defines that hint that we are compiling for 32-bit x86.
+#ifndef __i386
+#if defined(__i386__) || defined(i386) || defined(X86) || defined(_M_IX86)
+#define __i386
+#endif
+#endif // __i386
+
+// Should we use inline x86 assembler?
+#if defined(__i386) && defined(__GNUC__)
+#define _USE_X86_ASM
+#endif
 
 
 
 //************************************************************************
 //****                  GLFW internal functions                       ****
 //************************************************************************
+
+// Functions for accessing upper and lower parts of 64-bit integers
+// (Note: These are endian dependent, but ONLY used on x86 platforms!)
+#define _HIGH(x) ((unsigned int*)&x)[1]
+#define _LOW(x)  *((unsigned int*)&x)
+
+
+//========================================================================
+// _glfwCPUID() - Execute x86 CPUID instruction
+//========================================================================
+
+#ifdef _USE_X86_ASM
+
+static int _glfwCPUID( unsigned int ID, unsigned int *a, unsigned int *b,
+    unsigned int *c, unsigned int *d )
+{
+    int has_cpuid;
+    unsigned int local_a, local_b, local_c, local_d;
+
+    // Inline assembly - GCC version
+#if defined(__i386) && defined(__GNUC__)
+
+    // Detect CPUID support
+    asm(
+        "pushf\n\t"
+        "pop    %%eax\n\t"
+        "movl   %%eax,%%ebx\n\t"
+        "xorl   $0x00200000,%%eax\n\t"
+        "push   %%eax\n\t"
+        "popf\n\t"
+        "pushf\n\t"
+        "pop    %%eax\n\t"
+        "xorl   %%eax,%%ebx\n\t"
+        "movl   %%eax,%0\n\t"
+        : "=m" (has_cpuid)
+        :
+        : "%eax", "%ebx"
+    );
+    if( !has_cpuid )
+    {
+        return GL_FALSE;
+    }
+
+    // Execute CPUID
+    asm(
+        "movl   %4,%%eax\n\t"
+        "cpuid\n\t"
+        "movl   %%eax,%0\n\t"
+        "movl   %%ebx,%1\n\t"
+        "movl   %%ecx,%2\n\t"
+        "movl   %%edx,%3\n\t"
+        : "=m" (local_a), "=m" (local_b), "=m" (local_c), "=m" (local_d)
+        : "m" (ID)
+        : "%eax", "%ebx", "%ecx", "%edx"
+    );
+
+#endif
+
+    // Common code for all compilers
+#ifdef _USE_X86_ASM
+
+    *a = local_a;
+    *b = local_b;
+    *c = local_c;
+    *d = local_d;
+    return GL_TRUE;
+
+#else
+
+    return GL_FALSE;
+
+#endif // _USE_X86_ASM
+}
+
+#endif // _USE_X86_ASM
+
+
+//========================================================================
+// _glfwHasRDTSC() - Check for RDTSC availability AND usefulness
+//========================================================================
+
+static int _glfwHasRDTSC( void )
+{
+#ifdef _USE_X86_ASM
+
+    unsigned int cpu_name1, cpu_name2, cpu_name3;
+    unsigned int cpu_signature, cpu_brandID;
+    unsigned int max_base, feature_flags, has_rdtsc, has_htt, num_logical;
+    unsigned int dummy;
+    SYSTEM_INFO  si;
+
+    // Get processor vendor string (will return 0 if CPUID is not
+    // supported)
+    if( !_glfwCPUID( 0, &max_base, &cpu_name1, &cpu_name3, &cpu_name2 ) )
+    {
+        return GL_FALSE;
+    }
+
+    // Does the processor support base CPUID function 1?
+    if( max_base < 1 )
+    {
+        return GL_FALSE;
+    }
+
+    // Get CPU capabilities, CPU Brand ID & CPU Signature
+    _glfwCPUID( 1, &cpu_signature, &cpu_brandID, &dummy, &feature_flags );
+
+    // Is RDTSC supported?
+    if( feature_flags & 0x00000010 )
+    {
+        return GL_TRUE;
+    }
+    return GL_FALSE;
+
+#else
+
+    // Not a supported compiler
+    return GL_FALSE;
+
+#endif
+}
+
+
+//------------------------------------------------------------------------
+// _RDTSC() - Get CPU cycle count using the RDTSC instruction
+//------------------------------------------------------------------------
+
+#if defined(__i386) && defined(__GNUC__)
+
+// Read 64-bit processor Time Stamp Counter - GCC version
+#define _RDTSC( hi, lo ) \
+    asm( \
+        "rdtsc\n\t" \
+        "movl %%edx,%0\n\t" \
+        "movl %%eax,%1" \
+        : "=m" (hi), "=m" (lo) \
+        :  \
+        : "%edx", "%eax" \
+    );
+
+#else
+
+#define _RDTSC( hi, lo ) {hi=lo=0;}
+
+#endif
+
+
+
 
 //========================================================================
 // _glfwInitTimer() - Initialize timer
