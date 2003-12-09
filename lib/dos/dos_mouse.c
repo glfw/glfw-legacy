353d63fa343ae7a928d401888f2a09a600f7ab7b
Marcus Geelnard <marcus256@users.sourceforge.net>
2003-12-09 22:12:29 +0000

    More cleanups. Also removed window boundary check (do in dos_window.c if necessary!)

--- lib/dos/dos_mouse.c
+++ lib/dos/dos_mouse.c
@@ -31,7 +31,7 @@
 // Marcus Geelnard
 // marcus.geelnard at home.se
 //------------------------------------------------------------------------
-// $Id: dos_mouse.c,v 1.4 2003-12-08 23:26:54 marcus256 Exp $
+// $Id: dos_mouse.c,v 1.5 2003-12-09 22:12:29 marcus256 Exp $
 //========================================================================
 
 #include "internal.h"
@@ -39,31 +39,18 @@
 //========================================================================
 // Most of the code in this source file is based on the mouse driver in
 // Daniel Borca's GLUT implementation for DOS/MESA.
-//
-//                   !!! MUCH CLEANUP TO BE DONE !!!
-//
 //========================================================================
 
 //========================================================================
 // Definitions
 //========================================================================
 
-#define MIN(x,y)     (((x) < (y)) ? (x) : (y))
-#define MAX(x,y)     (((x) > (y)) ? (x) : (y))
-#define MID(x,y,z)   MAX((x), MIN((y), (z)))
-
-#define MOUSE_STACK_SIZE 16384
-
-#define CLEAR_MICKEYS() \
-    do { \
-        __asm __volatile ("movw $0xb, %%ax; int $0x33":::"%eax", "%ecx", "%edx"); \
-        _glfwMouseDrv.ox = _glfwMouseDrv.oy = 0; \
-    } while (0)
+#define _GLFW_MOUSE_STACK_SIZE 16384
 
 
 // Assembler function prototypes
-extern void mouse_wrap( void );
-extern int  *mouse_wrap_end;
+extern void _glfwMouseWrap( void );
+extern int  *_glfwMouseWrap_end;
 
 
 //========================================================================
@@ -71,15 +58,10 @@ extern int  *mouse_wrap_end;
 //========================================================================
 
 static struct {
-    volatile int x, y, z, b;
-    volatile int old_x, old_y, old_z, old_b;
-    long         mouse_callback;
+    long         Callback;
     __dpmi_regs  Regs;
-
-    int minx, maxx, miny, maxy, minz, maxz;
-    int ox, oy;
-    int emulat3;
-
+    int          Emulate3;
+    int          OldX, OldY, OldB;
 } _glfwMouseDrv;
 
 static int _glfwMouseInstalled = 0;
@@ -91,53 +73,66 @@ static int _glfwMouseInstalled = 0;
 //************************************************************************
 
 //========================================================================
-// _glfwMouseFunc() - Mouse interrupt callback function
+// _glfwMouseInt() - Mouse interrupt handler
 //========================================================================
 
-static void _glfwMouseFunc( int mouse_x, int mouse_y, int mouse_z,
-    int mouse_b )
+static void _glfwMouseInt( __dpmi_regs *r )
 {
+    int newx, newy, dx, dy, dz, buttons;
     _GLFWdosevent event;
     struct mousemove_event   *mousemove   = &event.MouseMove;
     struct mousewheel_event  *mousewheel  = &event.MouseWheel;
     struct mousebutton_event *mousebutton = &event.MouseButton;
 
-    /* mouse moved? */
-    if( (mouse_x != _glfwMouseDrv.old_x) ||
-        (mouse_y != _glfwMouseDrv.old_y) )
+    // Calculate mouse deltas
+    newx = (signed short)r->x.si;
+    newy = (signed short)r->x.di;
+    dx   = newx - _glfwMouseDrv.OldX;
+    dy   = newy - _glfwMouseDrv.OldY;
+    dz   = (signed char)r->h.bh;
+
+    // Get mouse buttons status
+    buttons = r->h.bl;
+
+    // Emulate 3rd mouse button?
+    if( _glfwMouseDrv.Emulate3 )
+    {
+        if( (buttons & 3) == 3 )
+        {
+            buttons = 4;
+        }
+    }
+
+    // Mouse moved?
+    if( dx || dy )
     {
         mousemove->Type = _GLFW_DOS_MOUSE_MOVE_EVENT;
-        mousemove->DeltaX = mouse_x - _glfwMouseDrv.old_x;
-        mousemove->DeltaY = mouse_y - _glfwMouseDrv.old_y;
+        mousemove->DeltaX = dx;
+        mousemove->DeltaY = dy;
         _glfwPostDOSEvent( &event );
-
-        _glfwMouseDrv.old_x = mouse_x;
-        _glfwMouseDrv.old_y = mouse_y;
     }
 
-    /* mouse wheel moved? */
-    if( mouse_z != _glfwMouseDrv.old_z )
+    // Mouse wheel moved?
+    if( dz )
     {
         mousewheel->Type = _GLFW_DOS_MOUSE_WHEEL_EVENT;
-        mousewheel->WheelDelta = mouse_z - _glfwMouseDrv.old_z;
+        mousewheel->WheelDelta = dz;
         _glfwPostDOSEvent( &event );
-
-        _glfwMouseDrv.old_z = mouse_z;
     }
 
-    /* button state changed? */
-    if( mouse_b != _glfwMouseDrv.old_b )
+    // Button state changed?
+    if( buttons != _glfwMouseDrv.OldB )
     {
         mousebutton->Type = _GLFW_DOS_MOUSE_BUTTON_EVENT;
 
         // Left mouse button changed?
-        if( (_glfwMouseDrv.old_b & 1) && !(mouse_b & 1) )
+        if( (_glfwMouseDrv.OldB & 1) && !(buttons & 1) )
         {
             mousebutton->Button = GLFW_MOUSE_BUTTON_LEFT;
             mousebutton->Action = GLFW_RELEASE;
             _glfwPostDOSEvent( &event );
         }
-        else if( !(_glfwMouseDrv.old_b & 1) && (mouse_b & 1) )
+        else if( !(_glfwMouseDrv.OldB & 1) && (buttons & 1) )
         {
             mousebutton->Button = GLFW_MOUSE_BUTTON_LEFT;
             mousebutton->Action = GLFW_PRESS;
@@ -145,13 +140,13 @@ static void _glfwMouseFunc( int mouse_x, int mouse_y, int mouse_z,
         }
 
         // Right mouse button changed?
-        if( (_glfwMouseDrv.old_b & 2) && !(mouse_b & 2) )
+        if( (_glfwMouseDrv.OldB & 2) && !(buttons & 2) )
         {
             mousebutton->Button = GLFW_MOUSE_BUTTON_RIGHT;
             mousebutton->Action = GLFW_RELEASE;
             _glfwPostDOSEvent( &event );
         }
-        else if( !(_glfwMouseDrv.old_b & 2) && (mouse_b & 2) )
+        else if( !(_glfwMouseDrv.OldB & 2) && (buttons & 2) )
         {
             mousebutton->Button = GLFW_MOUSE_BUTTON_RIGHT;
             mousebutton->Action = GLFW_PRESS;
@@ -159,82 +154,91 @@ static void _glfwMouseFunc( int mouse_x, int mouse_y, int mouse_z,
         }
 
         // Middle mouse button changed?
-        if( (_glfwMouseDrv.old_b & 4) && !(mouse_b & 4) )
+        if( (_glfwMouseDrv.OldB & 4) && !(buttons & 4) )
         {
             mousebutton->Button = GLFW_MOUSE_BUTTON_MIDDLE;
             mousebutton->Action = GLFW_RELEASE;
             _glfwPostDOSEvent( &event );
         }
-        else if( !(_glfwMouseDrv.old_b & 4) && (mouse_b & 4) )
+        else if( !(_glfwMouseDrv.OldB & 4) && (buttons & 4) )
         {
             mousebutton->Button = GLFW_MOUSE_BUTTON_MIDDLE;
             mousebutton->Action = GLFW_PRESS;
             _glfwPostDOSEvent( &event );
         }
 
-        _glfwMouseDrv.old_b = mouse_b;
-    }
-} ENDOFUNC(_glfwMouseFunc)
-
-
-//========================================================================
-// mouse() - Mouse interrupt handler
-//========================================================================
-
-static void mouse( __dpmi_regs *r )
-{
-    int newx, newy, dx, dy, dz;
-
-    // Calculate mouse deltas
-    newx = (signed short)r->x.si;
-    newy = (signed short)r->x.di;
-    dx = newx - _glfwMouseDrv.ox;
-    dy = newy - _glfwMouseDrv.oy;
-    dz = (signed char)r->h.bh;
-
-    // Remember old mouse position
-    _glfwMouseDrv.ox = newx;
-    _glfwMouseDrv.oy = newy;
-
-    // Store x,y,z & button state
-    _glfwMouseDrv.x = MID( _glfwMouseDrv.minx, _glfwMouseDrv.x + dx,
-                           _glfwMouseDrv.maxx);
-    _glfwMouseDrv.y = MID( _glfwMouseDrv.miny, _glfwMouseDrv.y + dy,
-                           _glfwMouseDrv.maxy);
-    _glfwMouseDrv.z = MID( _glfwMouseDrv.minz, _glfwMouseDrv.z + dz,
-                           _glfwMouseDrv.maxz);
-    _glfwMouseDrv.b = r->h.bl;
-
-    // Emulate 3rd mouse button?
-    if( _glfwMouseDrv.emulat3 )
-    {
-        if( (_glfwMouseDrv.b & 3) == 3 )
-        {
-            _glfwMouseDrv.b = 4;
-        }
     }
 
-    // Create new mouse events
-    _glfwMouseFunc( _glfwMouseDrv.x, _glfwMouseDrv.y, _glfwMouseDrv.z,
-                    _glfwMouseDrv.b );
-} ENDOFUNC(mouse)
+    // Remember old mouse state
+    _glfwMouseDrv.OldX = newx;
+    _glfwMouseDrv.OldY = newy;
+    _glfwMouseDrv.OldB = buttons;
+} ENDOFUNC(_glfwMouseInt)
 
 
 //========================================================================
-// _glfwInstallMouse() - Install mouse driver
+// _glfwMouseWrap()
 //========================================================================
 
-static int _glfwInstallMouse( void )
+// Hack alert: `_glfwMouseWrap_end' actually holds the address of stack in a
+// safe data selector.
+
+__asm("\n\
+                .text                           \n\
+                .p2align 5,,31                  \n\
+                .global __glfwMouseWrap         \n\
+__glfwMouseWrap:                                \n\
+                cld                             \n\
+                lodsl                           \n\
+                movl    %eax, %es:42(%edi)      \n\
+                addw    $4, %es:46(%edi)        \n\
+                pushl   %es                     \n\
+                movl    %ss, %ebx               \n\
+                movl    %esp, %esi              \n\
+                lss     %cs:__glfwMouseWrap_end, %esp\n\
+                pushl   %ss                     \n\
+                pushl   %ss                     \n\
+                popl    %es                     \n\
+                popl    %ds                     \n\
+                movl    ___djgpp_dos_sel, %fs   \n\
+                pushl   %fs                     \n\
+                popl    %gs                     \n\
+                pushl   %edi                    \n\
+                call    _mouse                  \n\
+                popl    %edi                    \n\
+                movl    %ebx, %ss               \n\
+                movl    %esi, %esp              \n\
+                popl    %es                     \n\
+                iret                            \n\
+                .global __glfwMouseWrap_end         \n\
+__glfwMouseWrap_end:.long   0, 0");
+
+
+
+//************************************************************************
+//****                  GLFW internal functions                       ****
+//************************************************************************
+
+//========================================================================
+// _glfwInitMouse() - Initialize mouse driver
+//========================================================================
+
+int _glfwInitMouse( void )
 {
     int buttons;
 
-    /* fail if already call-backed */
-    if( _glfwMouseDrv.mouse_callback )
+    // Already installed?
+    if( _glfwMouseInstalled )
     {
         return 0;
     }
 
-    /* reset mouse and get status */
+    // Lock data and functions
+    LOCKDATA( _glfwMouseDrv );
+    LOCKFUNC( _glfwMouseInt );
+    LOCKFUNC( _glfwMouseWrap );
+
+    // Reset mouse and get status
     __asm("\n\
                 xorl    %%eax, %%eax    \n\
                 int     $0x33           \n\
@@ -246,21 +250,20 @@ static int _glfwInstallMouse( void )
         return 0;
     }
 
-    /* lock wrapper */
-    LOCKFUNC(mouse);
-    LOCKFUNC(mouse_wrap);
+    // Emulate third mouse button?
+    _glfwMouseDrv.Emulate3 = buttons < 3;
 
-    mouse_wrap_end[1] = __djgpp_ds_alias;
+    _glfwMouseWrap_end[1] = __djgpp_ds_alias;
 
-    /* grab a locked stack */
-    mouse_wrap_end[0] = (int)malloc( MOUSE_STACK_SIZE );
-    if( mouse_wrap_end[0] == NULL )
+    // Grab a locked stack
+    _glfwMouseWrap_end[0] = (int)malloc( _GLFW_MOUSE_STACK_SIZE );
+    if( _glfwMouseWrap_end[0] == NULL )
     {
         return 0;
     }
-    LOCKBUFF( mouse_wrap_end[0], MOUSE_STACK_SIZE );
+    LOCKBUFF( _glfwMouseWrap_end[0], _GLFW_MOUSE_STACK_SIZE );
 
-    /* try to hook a call-back */
+    // Try to hook a call-back
     __asm("\n\
                 pushl   %%ds            \n\
                 pushl   %%es            \n\
@@ -277,132 +280,34 @@ static int _glfwInstallMouse( void )
                 movw    %%dx, %%cx      \n\
                 movl    %%ecx, %0       \n\
         0:                              \n\
-    ":"=g"(_glfwMouseDrv.mouse_callback)
-    :"S" (mouse_wrap), "D"(&_glfwMouseDrv.Regs)
+    ":"=g"(_glfwMouseDrv.Callback)
+    :"S" (_glfwMouseWrap), "D"(&_glfwMouseDrv.Regs)
     :"%eax", "%ecx", "%edx");
-    if( !_glfwMouseDrv.mouse_callback )
+    if( !_glfwMouseDrv.Callback )
     {
-        free( (void *)mouse_wrap_end[0] );
+        free( (void *)_glfwMouseWrap_end[0] );
         return 0;
     }
 
-    /* adjust stack */
-    mouse_wrap_end[0] += MOUSE_STACK_SIZE;
+    // Adjust stack
+    _glfwMouseWrap_end[0] += _GLFW_MOUSE_STACK_SIZE;
 
-    /* install the handler */
+    // Install the handler
     _glfwMouseDrv.Regs.x.ax = 0x000c;
     _glfwMouseDrv.Regs.x.cx = 0x7f | 0x80;
-    _glfwMouseDrv.Regs.x.dx = _glfwMouseDrv.mouse_callback & 0xffff;
-    _glfwMouseDrv.Regs.x.es = _glfwMouseDrv.mouse_callback >> 16;
-    __dpmi_int(0x33, &_glfwMouseDrv.Regs);
+    _glfwMouseDrv.Regs.x.dx = _glfwMouseDrv.Callback & 0xffff;
+    _glfwMouseDrv.Regs.x.es = _glfwMouseDrv.Callback >> 16;
+    __dpmi_int( 0x33, &_glfwMouseDrv.Regs );
 
-    CLEAR_MICKEYS();
+    // Clear mickies
+    __asm __volatile ("\n\
+        movw $0xb, %%ax;    \n\
+        int $0x33           \n\
+    ":::"%eax", "%ecx", "%edx");
 
-    _glfwMouseDrv.emulat3 = buttons<3;
-
-    return buttons;
-}
-
-
-//========================================================================
-// _glfwRemoveMouse() - Uninstall mouse driver
-//========================================================================
-
-static void _glfwRemoveMouse( void )
-{
-    if( _glfwMouseDrv.mouse_callback )
-    {
-        __asm("\n\
-                movl    %%edx, %%ecx    \n\
-                shrl    $16, %%ecx      \n\
-                movw    $0x0304, %%ax   \n\
-                int     $0x31           \n\
-                movw    $0x000c, %%ax   \n\
-                xorl    %%ecx, %%ecx    \n\
-                int     $0x33           \n\
-        "::"d"(_glfwMouseDrv.mouse_callback):"%eax", "%ecx");
-
-        _glfwMouseDrv.mouse_callback = 0;
-
-        free( (void *)(mouse_wrap_end[0] - MOUSE_STACK_SIZE) );
-    }
-}
-
-
-//========================================================================
-// mouse_wrap()
-//========================================================================
-
-/* Hack alert:
- * `mouse_wrap_end' actually holds the
- * address of stack in a safe data selector.
- */
-__asm("\n\
-                .text                           \n\
-                .p2align 5,,31                  \n\
-                .global _mouse_wrap             \n\
-_mouse_wrap:                                    \n\
-                cld                             \n\
-                lodsl                           \n\
-                movl    %eax, %es:42(%edi)      \n\
-                addw    $4, %es:46(%edi)        \n\
-                pushl   %es                     \n\
-                movl    %ss, %ebx               \n\
-                movl    %esp, %esi              \n\
-                lss     %cs:_mouse_wrap_end, %esp\n\
-                pushl   %ss                     \n\
-                pushl   %ss                     \n\
-                popl    %es                     \n\
-                popl    %ds                     \n\
-                movl    ___djgpp_dos_sel, %fs   \n\
-                pushl   %fs                     \n\
-                popl    %gs                     \n\
-                pushl   %edi                    \n\
-                call    _mouse                  \n\
-                popl    %edi                    \n\
-                movl    %ebx, %ss               \n\
-                movl    %esi, %esp              \n\
-                popl    %es                     \n\
-                iret                            \n\
-                .global _mouse_wrap_end         \n\
-_mouse_wrap_end:.long   0, 0");
-
-
-
-//************************************************************************
-//****                  GLFW internal functions                       ****
-//************************************************************************
-
-//========================================================================
-// _glfwInitMouse() - Initialize mouse driver
-//========================================================================
-
-int _glfwInitMouse( void )
-{
-    if( _glfwMouseInstalled )
-    {
-        return 0;
-    }
-
-    // Lock data and functions
-    LOCKDATA( _glfwMouseDrv );
-    LOCKFUNC( _glfwMouseFunc );
-
-    // Setup initial mouse driver settings
-    _glfwMouseDrv.minx       = 0;
-    _glfwMouseDrv.maxx       = _glfwWin.Width-1;
-    _glfwMouseDrv.miny       = 0;
-    _glfwMouseDrv.maxy       = _glfwWin.Height-1;
-    _glfwMouseDrv.minz       = 0;
-    _glfwMouseDrv.maxz       = 255;
-    _glfwMouseDrv.emulat3    = 0;
-
-    // Install mouse driver
-    _glfwMouseInstalled = _glfwInstallMouse();
-    if( !_glfwMouseInstalled )
-    {
-        return 0;
-    }
+    _glfwMouseDrv.OldX = 0;
+    _glfwMouseDrv.OldY = 0;
+    _glfwMouseDrv.OldB = 0;
 
     return 1;
 }
@@ -414,9 +319,23 @@ int _glfwInitMouse( void )
 
 void _glfwTerminateMouse( void )
 {
-    if( _glfwMouseInstalled )
+    if( !_glfwMouseInstalled )
     {
-        _glfwRemoveMouse();
-        _glfwMouseInstalled = 0;
+        return;
     }
+
+    __asm("\n\
+            movl    %%edx, %%ecx    \n\
+            shrl    $16, %%ecx      \n\
+            movw    $0x0304, %%ax   \n\
+            int     $0x31           \n\
+            movw    $0x000c, %%ax   \n\
+            xorl    %%ecx, %%ecx    \n\
+            int     $0x33           \n\
+    "::"d"(_glfwMouseDrv.Callback):"%eax", "%ecx");
+    _glfwMouseDrv.Callback = 0;
+
+    free( (void *)(_glfwMouseWrap_end[0] - _GLFW_MOUSE_STACK_SIZE) );
+
+    _glfwMouseInstalled = 0;
 }
