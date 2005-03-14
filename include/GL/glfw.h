61b012da26d0e384c33b298b6c5146c95e0c398a
Marcus Geelnard <marcus256@users.sourceforge.net>
2005-03-14 20:52:51 +0000

    New year

--- include/GL/glfw.h
+++ include/GL/glfw.h
@@ -5,7 +5,7 @@
 // Author:      Marcus Geelnard (marcus.geelnard at home.se)
 // WWW:         http://glfw.sourceforge.net
 //------------------------------------------------------------------------
-// Copyright (c) 2002-2004 Marcus Geelnard
+// Copyright (c) 2002-2005 Marcus Geelnard
 //
 // This software is provided 'as-is', without any express or implied
 // warranty. In no event will the authors be held liable for any damages
@@ -29,7 +29,7 @@
 // Marcus Geelnard
 // marcus.geelnard at home.se
 //------------------------------------------------------------------------
-// $Id: glfw.h,v 1.15 2004-08-31 19:22:03 marcus256 Exp $
+// $Id: glfw.h,v 1.16 2005-03-14 20:52:51 marcus256 Exp $
 //========================================================================
 
 #ifndef __glfw_h_
