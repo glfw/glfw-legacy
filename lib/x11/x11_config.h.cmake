1fb6dbe24a3fe23cc1beb2d0590b7a3bce342a44
Camilla Berglund <elmindreda@elmindreda.org>
2008-07-25 00:38:01 +0000

    Removing stuff.

--- lib/x11/x11_config.h.cmake
+++ lib/x11/x11_config.h.cmake
@@ -10,22 +10,5 @@
 #cmakedefine _GLFW_HAS_GLXGETPROCADDRESSARB 1 
 /* Define this to 1 if glxGetProcAddressEXT is available */
 #cmakedefine _GLFW_HAS_GLXGETPROCADDRESSEXT 1 
-/* Define this to 1 if Pthreads are available */ 
-#cmakedefine _GLFW_HAS_PTHREAD     1 
-#cmakedefine _REENTRANT 
-/* Define this to 1 if Sched Yield is available */ 
-#cmakedefine _GLFW_HAS_SCHED_YIELD 1
-/* Define this to 1 if dlopen is available */ 
-#cmakedefine _GLFW_HAS_DLOPEN      1 
-/* 
- Define this to 1 if Sysconf is available and can 
- be used to get the number of CPU available 
-*/ 
-#cmakedefine _GLFW_HAS_SYSCONF     1
-/* 
- Define this to 1 if Sysctl is available 
- and can be used to get the number of CPU available 
-*/
-#cmakedefine _GLFW_HAS_SYSCTL      1
 
 
