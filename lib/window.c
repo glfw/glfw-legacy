03c1acd34a31acc124abc011e29b20757ffba193
Camilla Berglund <elmindreda@elmindreda.org>
2009-10-07 10:30:54 +0000

    Removed unused local variable.

--- lib/window.c
+++ lib/window.c
@@ -303,7 +303,6 @@ GLFWAPI int GLFWAPIENTRY glfwOpenWindow( int width, int height,
     int redbits, int greenbits, int bluebits, int alphabits,
     int depthbits, int stencilbits, int mode )
 {
-    int x;
     _GLFWhints hints;
     _GLFWfbconfig fbconfig;
 
