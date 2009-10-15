44763c03e1bbb816eddea423c870fa4f6237bf59
Camilla Berglund <elmindreda@elmindreda.org>
2009-10-15 21:00:58 +0000

    Added context attribute flags.

--- tests/version.c
+++ tests/version.c
@@ -31,25 +31,42 @@
 
 #include <GL/glfw.h>
 
+#ifndef GL_VERSION_3_2
+#define GL_CONTEXT_PROFILE_MASK 0x9126
+#endif
+
+#ifdef _WIN32
+#define strcasecmp(x, y) stricmp(x, y)
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "getopt.h"
 
 static void usage(void)
 {
-    printf("version [-h] [-m MAJOR] [-n MINOR]\n");
+    printf("version [-h] [-m MAJOR] [-n MINOR] [-d] [-f] [-p PROFILE]\n");
 }
 
 int main(int argc, char** argv)
 {
     const GLubyte* extensions;
-    int ch, major = 0, minor = 0, revision;
+    int ch, profile = 0, major = 0, minor = 0, revision;
+    GLboolean debug = GL_FALSE, forward = GL_FALSE;
+    GLint mask;
 
-    while ((ch = getopt(argc, argv, "hm:n:")) != -1)
+    while ((ch = getopt(argc, argv, "dfhm:n:p:")) != -1)
     {
         switch (ch)
         {
+            case 'd':
+                debug = GL_TRUE;
+                break;
+            case 'f':
+                forward = GL_TRUE;
+                break;
             case 'h':
                 usage();
                 exit(0);
@@ -58,6 +75,17 @@ int main(int argc, char** argv)
                 break;
             case 'n':
                 minor = atoi(optarg);
+                break;
+            case 'p':
+                if (strcasecmp(optarg, "core") == 0)
+                    profile = GLFW_OPENGL_CORE_PROFILE;
+                else if (strcasecmp(optarg, "compat") == 0)
+                    profile = GLFW_OPENGL_COMPAT_PROFILE;
+                else
+                {
+                    usage();
+                    exit(1);
+                }
                 break;
             default:
                 usage();
@@ -79,6 +107,15 @@ int main(int argc, char** argv)
 
     if (minor > 0)
         glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, minor);
+
+    if (debug)
+        glfwOpenWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
+
+    if (forward)
+        glfwOpenWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    if (profile != 0)
+        glfwOpenWindowHint(GLFW_OPENGL_PROFILE, profile);
 
     // We assume here that we stand a better chance of success by leaving all
     // possible details of pixel format selection to GLFW
@@ -108,6 +145,12 @@ int main(int argc, char** argv)
     glfwGetGLVersion(&major, &minor, &revision);
 
     printf("OpenGL context version parsed by GLFW: %u.%u.%u\n", major, minor, revision);
+
+    if (major > 3 || (major == 3 && minor >= 2))
+    {
+        glGetIntegerv(GL_CONTEXT_PROFILE_MASK, &mask);
+        printf("OpenGL profile mask: 0x%08x\n", mask);
+    }
 
     printf("OpenGL context version string: \"%s\"\n", glGetString(GL_VERSION));
     printf("OpenGL context renderer string: \"%s\"\n", glGetString(GL_RENDERER));
