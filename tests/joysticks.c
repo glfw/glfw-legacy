2f74061fb19935ca8c02e28c3f50850a85aeea63
Camilla Berglund <elmindreda@elmindreda.org>
2009-10-19 23:41:30 +0000

    Formatting.

--- tests/joysticks.c
+++ tests/joysticks.c
@@ -3,9 +3,10 @@
  * joystick input test.
  *========================================================================*/
 
+#include <GL/glfw.h>
+
 #include <stdio.h>
 #include <math.h>
-#include <GL/glfw.h>
 
 #define MAX_AXES     10
 #define MAX_BUTTONS  30
@@ -21,40 +22,21 @@ struct JoystickState
 
 static struct JoystickState states[GLFW_JOYSTICK_LAST + 1];
 
-
-
 int running;
 int keyrepeat  = 0;
 int systemkeys = 1;
 
 
 /*========================================================================
- * keyfun()
- *========================================================================*/
-
-void keyfun( int key, int action )
-{
-    if( action != GLFW_PRESS )
-    {
-        return;
-    }
-
-    if (key == GLFW_KEY_ESC)
-    {
-        printf( "ESC => quit program\n" );
-        running = GL_FALSE;
-    }
-}
-
-/*========================================================================
  * Retrieve joystick states
  *========================================================================*/
-void updateJoysticksState( void )
+static void updateJoysticksState(void)
 {
     int joy;
-    for ( joy = GLFW_JOYSTICK_1 ; joy < GLFW_JOYSTICK_LAST + 1 ; ++joy )
+
+    for (joy = GLFW_JOYSTICK_1;  joy < GLFW_JOYSTICK_LAST + 1;  joy++)
     {
-        printf( "Updating information for joystick %d\n", joy );
+        printf("Updating information for joystick %d\n", joy);
         states[joy].present = glfwGetJoystickParam(joy, GLFW_PRESENT);
         if (states[joy].present == GL_TRUE)
         {
@@ -67,92 +49,83 @@ void updateJoysticksState( void )
 /*========================================================================
  * Print out the state of all joysticks on the standard output
  *========================================================================*/
-void displayJoysticksState( void )
+static void displayJoysticksState(void)
 {
     int joy;
     int i;
-    for ( joy = GLFW_JOYSTICK_1 ; joy < GLFW_JOYSTICK_LAST + 1 ; ++joy )
+
+    for (joy = GLFW_JOYSTICK_1;  joy < GLFW_JOYSTICK_LAST + 1;  joy++)
     {
-        printf( "Joystick %d: %s\n", joy, (states[joy].present == GL_TRUE ? "present" : "not connected"));
+        printf("Joystick %d: %s\n", joy, (states[joy].present == GL_TRUE ? "present" : "not connected"));
+
         if (states[joy].present == GL_TRUE)
         {
             if (states[joy].num_axes > 0)
             {
-                printf( "  axes: %.3f", states[joy].axes[0] );
-                for ( i = 1 ; i < states[joy].num_axes ; ++i )
-                {
-                    printf( ", %.3f", states[joy].axes[i] );
-                }
-                printf( "\n" );
+                printf("  axes: %.3f", states[joy].axes[0]);
+                for (i = 1;  i < states[joy].num_axes;  i++)
+                    printf(", %.3f", states[joy].axes[i]);
+
+                printf("\n");
             }
             else
-            {
-                printf( "  axes: none\n" );
-            }
+                printf("  axes: none\n");
 
             if (states[joy].num_buttons > 0)
             {
-                printf( "  buttons: 00 => %c", ( ( states[joy].buttons[0] == GLFW_PRESS ) ? 'P' : 'R' ) );
-                for ( i = 1 ; i < states[joy].num_buttons ; ++i )
-                {
-                    printf ( ", %02d => %c", i, ( ( states[joy].buttons[i] == GLFW_PRESS ) ? 'P' : 'R' ) );
-                }
-                printf( "\n" );
+                printf("  buttons: 00 => %c", ((states[joy].buttons[0] == GLFW_PRESS) ? 'P' : 'R'));
+
+                for (i = 1;  i < states[joy].num_buttons;  i++)
+                    printf(", %02d => %c", i, ((states[joy].buttons[i] == GLFW_PRESS) ? 'P' : 'R'));
+
+                printf("\n");
             }
             else
-            {
-                printf( "  buttons: none\n" );
-            }
+                printf("  buttons: none\n");
         }
     }
 }
 
-/*========================================================================
- * main()
- *========================================================================*/
-
-int main( void )
+int main(void)
 {
-    double  start;
-    double  t;
-    double  update;
+    double start;
+    double t;
+    double update;
 
     /* Initialise GLFW */
     glfwInit();
-    printf( "The program will work for 20 seconds and display every seconds the state of the joysticks\n" );
-    printf( "Your computer is going to be very slow as the program is doing an active loop .....\n" );
+    printf("The program will work for 20 seconds and display every seconds the state of the joysticks\n");
+    printf("Your computer is going to be very slow as the program is doing an active loop .....\n");
 
     start = glfwGetTime();
     update = start;
 
     /* print the initial state of all joysticks */
     updateJoysticksState();
-    printf( "\n" );
+    printf("\n");
     displayJoysticksState();
 
     running = GL_TRUE;
 
     /* Main loop */
-    while( running )
+    while (running)
     {
         /* Get time */
         t = glfwGetTime();
 
         /* Display the state of all connected joysticks every secons */
-        if ( ( t - update ) > 1.0 )
+        if ((t - update) > 1.0)
         {
             update = t;
-            printf( "\n" );
+            printf("\n");
             updateJoysticksState();
-            printf( "\n" );
+            printf("\n");
             displayJoysticksState();
         }
 
         /* Check if the window was closed */
-        if ( ( t - start ) > 20.0 )
-        {
+        if ((t - start) > 20.0)
             running = GL_FALSE;
-        }
     }
 
     /* Close OpenGL window and terminate GLFW */
@@ -160,3 +133,4 @@ int main( void )
 
     return 0;
 }
+
