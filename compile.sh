22916a29238bbe23cc8cb36fe4c49adb848e3e42
Camilla Berglund <elmindreda@elmindreda.org>
2009-10-30 22:55:52 +0000

    Removed elipses.

--- compile.sh
+++ compile.sh
@@ -589,9 +589,9 @@ GLFW_BIN_LFLAGS="-lGLU $LIBS -lm"
 
 MKNAME='./lib/x11/Makefile.x11'
 
-echo "Creating ""$MKNAME""..." 1>&6
+echo "Creating ""$MKNAME" 1>&6
 
-echo "$self: Creating ""$MKNAME""..." >&5
+echo "$self: Creating ""$MKNAME" >&5
 
 cat > "$MKNAME" <<EOF
 ##########################################################################
@@ -612,9 +612,9 @@ cat './lib/x11/Makefile.x11.in' >>$MKNAME
 
 MKNAME='./examples/Makefile.x11'
 
-echo "Creating ""$MKNAME""..." 1>&6
+echo "Creating ""$MKNAME" 1>&6
 
-echo "$self: Creating ""$MKNAME""..." >&5
+echo "$self: Creating ""$MKNAME" >&5
 
 cat > "$MKNAME" <<EOF
 ##########################################################################
@@ -635,9 +635,9 @@ cat './examples/Makefile.x11.in' >>$MKNAME
 
 MKNAME='./tests/Makefile.x11'
 
-echo "Creating ""$MKNAME""..." 1>&6
+echo "Creating ""$MKNAME" 1>&6
 
-echo "$self: Creating ""$MKNAME""..." >&5
+echo "$self: Creating ""$MKNAME" >&5
 
 cat > "$MKNAME" <<EOF
 ##########################################################################
@@ -658,9 +658,9 @@ cat './tests/Makefile.x11.in' >>$MKNAME
 
 MKNAME="./lib/x11/libglfw.pc.in"
 
-echo "Creating ""$MKNAME""..." 1>&6
+echo "Creating ""$MKNAME" 1>&6
 
-echo "$self: Creating ""$MKNAME""..." >&5
+echo "$self: Creating ""$MKNAME" >&5
 
 cat > "$MKNAME" <<EOF
 prefix=@PREFIX@
