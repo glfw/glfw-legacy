90329cc1fd7008898b133ac28141c84f953d2cab
Camilla Berglund <elmindreda@elmindreda.org>
2010-08-08 15:49:59 +0000

    Restored partial copyright history for Carbon port.

--- lib/carbon/carbon_enable.c
+++ lib/carbon/carbon_enable.c
@@ -5,6 +5,8 @@
 // WWW:         http://www.glfw.org/
 //------------------------------------------------------------------------
 // Copyright (c) 2002-2006 Marcus Geelnard
+// Copyright (c) 2003      Keith Bauer
+// Copyright (c) 2006-2007 Robin Leffmann
 // Copyright (c) 2006-2010 Camilla Berglund <elmindreda@elmindreda.org>
 //
 // This software is provided 'as-is', without any express or implied
