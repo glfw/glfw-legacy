2f93f1285a6b8ee4d4ca19914ae5637fa0bdccab
Camilla Berglund <elmindreda@elmindreda.org>
2012-07-02 14:16:43 +0000

    Fixed warning on MinGW-w64.

--- lib/win32/win32_init.c
+++ lib/win32/win32_init.c
@@ -350,7 +350,7 @@ int _glfwPlatformTerminate( void )
 
     // Restore FOREGROUNDLOCKTIMEOUT system setting
     SystemParametersInfo( SPI_SETFOREGROUNDLOCKTIMEOUT, 0,
-                          (LPVOID) _glfwLibrary.Sys.foregroundLockTimeout,
+                          (LPVOID) (INT_PTR) _glfwLibrary.Sys.foregroundLockTimeout,
                           SPIF_SENDCHANGE );
 
     return GL_TRUE;
