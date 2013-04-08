bd070c1256224882ab826dcb17f28dfa7990970a
Camilla Berglund <elmindreda@elmindreda.org>
2013-04-08 20:35:01 +0000

    Updated version to 2.7.9.

--- support/pascal/glfw.pas
+++ support/pascal/glfw.pas
@@ -55,7 +55,7 @@ const
   //========================================================================
   GLFW_VERSION_MAJOR    = 2;
   GLFW_VERSION_MINOR    = 7;
-  GLFW_VERSION_REVISION = 8;
+  GLFW_VERSION_REVISION = 9;
 
   //========================================================================
   // Input handling definitions
